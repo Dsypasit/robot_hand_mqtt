ad3015e80704e0879b471573958fa561010be384
Pasit Sri-intarasut <ong.pasit@gmail.com>
2022-05-20 21:48:30 +0700

    feat: add ui and connect mqtt

---
+++ lib/MQTTManager.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:robot_hand/MQTTAppState.dart';
+
+class MQTTManager{
+  late MqttServerClient client;
+
+  final MQTTAppState  _currentState;
+
+  MQTTManager({
+    required MQTTAppState state
+  }): _currentState = state;
+
+  // using async tasks, so the connection won't hinder the code flow
+  void prepareMqttClient() async {
+    _setupMqttClient();
+    await _connectClient();
+    _subscribeToTopic('Dart/Mqtt_client/testtopic');
+    publishMessage('Hello');
+  }
+
+  // waiting for the connection, if an error occurs, print it and disconnect
+  Future<void> _connectClient() async {
+    try {
+      print('client connecting....');
+      _currentState.setAppConnectionState(MQTTAppConnectionState.connecting);
+      await client.connect();
+      print('connecting success');
+    } on Exception catch (e) {
+      print('client exception - $e');
+      _currentState.setAppConnectionState(MQTTAppConnectionState.disconnected);
+      client.disconnect();
+    }
+
+    // when connected, print a confirmation, else print an error
+    if (client.connectionStatus?.state == MqttConnectionState.connected) {
+      _currentState.setAppConnectionState(MQTTAppConnectionState.connected);
+      print('client connected');
+    } else {
+      print(
+          'ERROR client connection failed - disconnecting, status is ${client.connectionStatus}');
+      _currentState.setAppConnectionState(MQTTAppConnectionState.disconnected);
+      client.disconnect();
+    }
+  }
+
+  void _setupMqttClient() {
+    client = MqttServerClient('public.mqtthq.com', 'Robotkub');
+    // the next 2 lines are necessary to connect with tls, which is used by HiveMQ Cloud
+    // client.secure = true;
+    // client.securityContext = SecurityContext.defaultContext;
+    client.keepAlivePeriod = 20;
+    client.onDisconnected = _onDisconnected;
+    client.onConnected = _onConnected;
+    client.onSubscribed = _onSubscribed;
+  }
+
+  void _subscribeToTopic(String topicName) {
+    print('Subscribing to the $topicName topic');
+    client.subscribe(topicName, MqttQos.atMostOnce);
+
+    // print the message when it is received
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+      var message = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+      print('YOU GOT A NEW MESSAGE:');
+      print(message);
+    });
+  }
+
+  void publishMessage(String message) {
+    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+
+    print('Publishing message "$message" to topic ${'Dart/Mqtt_client/testtopic'}');
+    client.publishMessage('Dart/Mqtt_client/testtopic', MqttQos.exactlyOnce, builder.payload!);
+  }
+
+  // callbacks for different events
+  void _onSubscribed(String topic) {
+    print('Subscription confirmed for topic $topic');
+  }
+
+  void _onDisconnected() {
+    print('OnDisconnected client callback - Client disconnection');
+    _currentState.setAppConnectionState(MQTTAppConnectionState.disconnected);
+  }
+
+  void _onConnected() {
+    _currentState.setAppConnectionState(MQTTAppConnectionState.connected);
+    print('OnConnected client callback - Client connection was sucessful');
+  }
+
+}
---
+++ lib/demo.dart
@@ -0,0 +1,168 @@
+import 'dart:io';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:robot_hand/MQTTManager.dart';
+import 'package:robot_hand/MQTTAppState.dart';
+
+
+class Demo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return _DemoState();
+  }
+}
+
+class _DemoState extends State<Demo>{
+  final TextEditingController _xAxisController = TextEditingController();
+  final TextEditingController _yAxisController = TextEditingController();
+  final TextEditingController _zAxisController = TextEditingController();
+
+  late MQTTAppState currentState;
+  late MQTTManager manager;
+
+  int? _result;
+
+  @override
+  void initSate(){
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _xAxisController.dispose();
+    _yAxisController.dispose();
+    _zAxisController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    final MQTTAppState appState = Provider.of<MQTTAppState>(context);
+    currentState = appState;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Robot hand Controller'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _xAxisController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'X axis',
+                icon: Icon(Icons.list_outlined),
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: _yAxisController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Y axis',
+                icon: Icon(Icons.list_outlined),
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              controller: _zAxisController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Z axis',
+                icon: Icon(Icons.list_outlined),
+              ),
+            ),
+            SizedBox(height: 20,),
+            _buildConnecteButtonFrom(currentState.getAppConnectionState),
+            SizedBox(height: 20,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  color: Colors.pinkAccent,
+                  child: Text(
+                    "Calculate",
+                    style: TextStyle(color: Colors.white)
+                  ),
+                  onPressed: currentState.getAppConnectionState == MQTTAppConnectionState.connected
+                   ?calculateRobot
+                   : null,
+                ),
+              ],
+            ),
+            SizedBox(height:30),
+            Text(
+              _result == null ? "Enter Value": "${_result.toString()}"
+            )
+          ],
+        )
+      )
+    );
+  }
+
+  Widget _buildConnecteButtonFrom(MQTTAppConnectionState state) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          // ignore: deprecated_member_use
+          child: RaisedButton(
+            color: Colors.lightBlueAccent,
+            child: const Text('Connect'),
+            onPressed: state == MQTTAppConnectionState.disconnected
+                ? connect
+                : null, //
+          ),
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          // ignore: deprecated_member_use
+          child: RaisedButton(
+            color: Colors.redAccent,
+            child: const Text('Disconnect'),
+            onPressed: state == MQTTAppConnectionState.connected
+                ? disconnect
+                : null, //
+          ),
+        ),
+      ],
+    );
+  }
+
+  void calculateRobot (){
+    final String message = _xAxisController.text + "," + _yAxisController.text + "," + _zAxisController.text;
+    print(message);
+    manager.publishMessage(message);
+  }
+
+  void disconnect (){
+    if (_result == null){
+      _result = 1;
+      setState(() {});
+      return;
+    }
+    _result = _result! + 1;
+    setState(() {});
+  }
+
+  String _prepareStateMessageFrom(MQTTAppConnectionState state) {
+    switch (state) {
+      case MQTTAppConnectionState.connected:
+        return 'Connected';
+      case MQTTAppConnectionState.connecting:
+        return 'Connecting';
+      case MQTTAppConnectionState.disconnected:
+        return 'Disconnected';
+    }
+  }
+
+  void connect(){
+    manager = MQTTManager(state: currentState);
+    manager.prepareMqttClient();
+  }
+
+}
